c3183cdd173c10e2e0bfd532fc3734f826eeb4b7
helenrutkovska <helen.rutkovska@gmail.com>
2022-06-03 07:46:38 +0200

    firstcommit

---
+++ src/p1/JavaClass.java
@@ -0,0 +1,45 @@
+package p1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaClass {
+    public static void main(String[] args) {
+        {
+            System.setProperty("webdriver.chrome.driver","D:\\Auto QA\\Chrome Driver\\chromedriver.exe");
+
+            WebDriver driver=new ChromeDriver();
+
+            driver.manage().window().maximize();
+
+            driver.get("https://store.steampowered.com");
+            //driver.findElement(By.xpath("//a[contains(@href,'about')]")).click();
+            driver.findElement(By.xpath("//a[contains(@class,'menuitem') and contains(text(),'ABOUT')]")).click();
+            //driver.findElement(By.xpath("//a[contains(@class,'menuitem') and contains(text(),'ABOUT')]")).click();
+
+            //Number of players online
+            WebElement online = driver.findElement(By.xpath("//div[@class='online_stat'][1]"));
+            String numberOnline = online.getText().replaceAll("[^\\.0123456789]","");
+            int playersOnline = Integer.parseInt(numberOnline);
+            //System.out.println(playersOnline);
+
+            //Number of players in a game
+            WebElement inGame = driver.findElement(By.xpath("//div[@class='online_stat'][2]"));
+            String numberPlayers = inGame.getText().replaceAll("[^\\.0123456789]","");
+            int playersInGame = Integer.parseInt(numberPlayers);
+            //System.out.println(playersInGame);
+
+            //Compare the numbers
+            if (playersOnline > playersInGame) {
+                System.out.println("Number of in-game players is less than number of players online");
+            } else {
+                System.out.println("Number of online players is less than number of in-game players");
+            }
+
+            driver.findElement(By.xpath("//a[contains(@class,'menuitem') and contains(text(),'STORE')]")).click();
+            driver.quit();
+        }
+    }
+}
